71c3fd5ea2c5e0fd01e51e67bd5ffc1abb582fea
Dan Loftus <dloftus98@yahoo.com>
2017-03-27 13:44:53 -0400

    update

--- runme.sh
+++ runme.sh
@@ -1,3 +1,12 @@
 cd /opt/netspectrum/aws/ubuntu-desktop
 
-cat (curl -Ls https://github.com/dloftus98/novnc-wss/self.pem) >self.pem
+curl -Ls https://github.com/dloftus98/novnc-wss/raw/master/self.pem >self.pem
+
+cd bin
+mv rc-local.sh rc-local.orig
+curl -Ls https://github.com/dloftus98/novnc-wss/raw/master/rc-local.sh >rc-local.sh
+
+echo Rebooting in 5 seconds
+sleep 5s
+sudo reboot
+
